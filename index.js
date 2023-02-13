8ae34f42f8d3a7259b91cc51a9ee86e7f8d37ed0
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-02-13 16:05:52 +0800

    scrim complete

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const modalEl = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentFrom = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const scaryMsg = document.getElementById('changing-header-text')
+const buttonContainer = document.getElementById('button-container')
 
 setTimeout(function() {
     modalEl.style.display = 'block'
 }, 3000) 
 
+declineBtn.addEventListener('mouseenter', function() {
+    console.log("Hey you're hovering on me")
+    buttonContainer.classList.toggle("reverse")
+  });
+
 
 consentFrom.addEventListener('submit', function(e) {
     e.preventDefault()
-    console.log("from submitted after preventDefult")
+    const consentFromData = new FormData(consentFrom)
+    console.log(consentFromData)
+    const username = consentFromData.get('user-name')
+
     modalText.innerHTML = ` <div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
     <p id="uploadText">
@@ -27,11 +38,14 @@ consentFrom.addEventListener('submit', function(e) {
         document.getElementById('main-title').style.display = 'none';
         consentFrom.style.display = 'none'
         modalText.innerHTML = ``
-        modalText.innerHTML = `<h2>Thanks you sucker! </h2>
+        modalText.innerHTML = `<h2>Thanks 
+        <span class="modal-display-name">${username}</span>, you sucker! 
+        </h2>
         <p>We just sold the rights to your soul.</p>
         <div class="idiot-gif">
             <img src="https://media0.giphy.com/media/2zelCiUo5KJyN8MgMr/giphy.gif?cid=ecf05e47slmz8vg81ewiy59ix89ini7oi7x8karc6coirfuh&rid=giphy.gif&ct=g">
         </div>`
+        modalCloseBtn.disabled = false
     
     }, 5000)
 })
